72b5a1dd9b9febf25610d871803f90ce5a7aa9bc
Dynisha Faust <dgeekgirl08@gmail.com>
2017-11-18 20:14:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function switchOnCharmFromTip(tipString){
   switch (tipString) {
   case "generous":
     return "Thank you so much."
-      break; 
-  case "not so generous":
+      break;
+  case "not as generous":
     return "Thank you."
     break;
-  
+
   default: "thanks for everything"
-    
+
    }
 }
